f4d55c1f073a6200af5248a9dc65f2b8acd8f42c
shimphillip <shimphillip@gmail.com>
2020-05-09 00:13:51 -0500

    header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './App.scss'
+import Header from './Header'
 
-function App() {
-  return <div className="App">App</div>
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.scss'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="heading">
+        Chuck Norris <span className="heading__special">Jokes</span>
+      </h1>
+    </header>
+  )
+}
+
+export default Header
